4a4e0a66590af20adb626f57d1a05da62a35bda6
Dan <daniel.munozdev@gmail.com>
2017-10-01 21:32:23 -0700

    Add if statement to switch #button text

--- js/blog.js
+++ js/blog.js
@@ -2,4 +2,9 @@ $('#button').click(function() {
   $('body').toggleClass('night-mode');
 
   console.log($(this).text());
+  if ($(this).text() === 'Switch to night mode') {
+    $('#button').text('Switch to day mode');
+  } else {
+    $('#button').text('Switch to night mode');
+  }
 });
